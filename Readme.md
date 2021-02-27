e7d4e42ed147fdf8838821b9ed3c6e24ffbe1c57
Piyush <piyushpatil98@protonmail.com>
2021-02-27 14:09:07 +0530

    Day 4:del Animation & change add Icon &

--- Readme.md
+++ Readme.md
@@ -4,22 +4,33 @@
 ## Wireframe  
 - https://dribbble.com/shots/4739703-To-do-List-Interaction-UI-Animation
 - [I(txt)] + [B_add]
-    - add => ( [B_check] + [txt>>-Task] + [B_delete] )-column
+    - add => ( [B_check] + [txt>>-Task] + [B_delete] )-column[drag-gable]
 
 ## Pseudo Code
-1. add button to add Complete Block with check,text,delete
+1. [add]:onclick button to add Complete Block with check,text,delete
     - [x] Get text from input
     - [x] When added Refresh input tab
     - [x] If input is blank then warn them !
-2. When click check , full block get strike
+2. [check]:onclick full block get strike
     - [x] First target check button 
     - [x] Then relate to its adjacent text to strike && Animate icon as complete
-    - [ ] Strike text with animation
-3. When click delete , full block get removed
+    - [ ] Strike text with animation with change Icon Animation
+3. [delete]:onclick full block get removed
     - [x] First target check button 
     - [x] Then relate to its parent
     - [x] And remove self (display)
-4. When Click task => You can edit that task 
+4. [task]:onclick => You can Edit that task 
+5. [task]:ondrag => You can darg up & Down
+    - [ ] Draggable block
+    - [ ] draggbale area with a Icon on left-side
+6. [task]:onhold => prioritize task
+    - [ ] Change icon to red,blue,green
+7. [list(+)]:onclick => Add a List
+7. Introduction Image slider for User Guide::https://in.pinterest.com/pin/480196379024476441/
+    - [ ] introduction Page Design
+    - [ ] on Page load Execution
+    - [ ] Slider Function
+8. User can Download those task as image
 
 
 ## Task
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let tar = document.querySelector('.main');
 let todo = document.querySelector("#todo");
-let help=document.querySelector(".help");
+let help = document.querySelector(".help");
 // If input is emplty then warn the user
 
 function display() {
@@ -12,46 +12,46 @@ function add() {
 
     var val = todo.value
     // if (task.length==0) {}
-    if(val.length==0){
+    if (val.length == 0) {
 
         help.style.display = 'block';
 
-    }else{
+    } else {
 
-    help.style.display = 'none';
-    // column
-    let column = document.createElement('div');
-    column.setAttribute('class', 'column list box is-full');
-    // span
-    let span1 = document.createElement('span');
-    span1.setAttribute('class', 'icon check is-medium');
-    let span2 = document.createElement('span');
-    span2.setAttribute('class', 'icon is-pulled-right del');
-    let spanTxt = document.createElement('span');
-    spanTxt.setAttribute('class', 'ml-2 val subtitle is-4');
-    // icon-text
-    let icon_text = document.createElement('span');
-    icon_text.setAttribute('class', 'icon-text');
-    // check-icon
-    let icon1 = document.createElement('img');
-    icon1.setAttribute('src', './cir.svg');
-    // text
-    let txt = document.createTextNode(val);
-    // delete-icon
-    let icon2 = document.createElement('img');
-    icon2.setAttribute('src', './delete.svg')
-    // Append >>
-    tar.appendChild(column);
-    column.appendChild(icon_text);
+        help.style.display = 'none';
+        // column
+        let column = document.createElement('div');
+        column.setAttribute('class', 'column list box is-full');
+        // span
+        let span1 = document.createElement('span');
+        span1.setAttribute('class', 'icon check is-medium');
+        let span2 = document.createElement('span');
+        span2.setAttribute('class', 'icon is-pulled-right del');
+        let spanTxt = document.createElement('span');
+        spanTxt.setAttribute('class', 'ml-2 val subtitle is-4');
+        // icon-text
+        let icon_text = document.createElement('span');
+        icon_text.setAttribute('class', 'icon-text');
+        // check-icon
+        let icon1 = document.createElement('img');
+        icon1.setAttribute('src', './cir.svg');
+        // text
+        let txt = document.createTextNode(val);
+        // delete-icon
+        let icon2 = document.createElement('img');
+        icon2.setAttribute('src', './delete.svg')
+        // Append >>
+        tar.appendChild(column);
+        column.appendChild(icon_text);
 
-    icon_text.appendChild(span1);
-    span1.appendChild(icon1);
+        icon_text.appendChild(span1);
+        span1.appendChild(icon1);
 
-    icon_text.appendChild(spanTxt);
-    spanTxt.appendChild(txt);
+        icon_text.appendChild(spanTxt);
+        spanTxt.appendChild(txt);
 
-    column.appendChild(span2);
-    span2.appendChild(icon2);
+        column.appendChild(span2);
+        span2.appendChild(icon2);
 
     }
     // column.appendChild(txt);
@@ -77,30 +77,49 @@ function add() {
             e.parentElement.nextElementSibling.classList.toggle('s')
         }
     })
-// Delete Button
-let del=document.querySelectorAll('.del')
-del.forEach(e=>{
-	e.onclick=()=>{
-	let c=e.parentElement.style.display = 'none';
-	console.log(c)
+    // Delete Button
+    let del = document.querySelectorAll('.del')
+    del.forEach(e => {
+        e.onclick = () => {
+            let c = e.parentElement.style
+            c.transition = '1s';
+            c.marginLeft = '100px';
+            c.opacity = '.1';
+            setTimeout(e=> {
+            c.display = 'none';
+            }, 1000);
+            console.log(c)
 
-	}
-})
-// Refresh Input
-todo.value = '';
+        }
+    })
+    // Refresh Input
+    todo.value = '';
+/*
+let box=document.querySelectorAll('.box')
+box.forEach(e=>{
+    e.addEventListener('touchmove',y=>{
 
-
-
-// Edit Task
-/*let txt=document.querySelectorAll('.val');
-txt.forEach(e=>{
-    e.onclick=()=>{
-        
-        e.classList.toggle("input")
-        e.classList.toggle("is-small")
-
-    }
+        e.style.transition = '1s';
+            e.style.marginLeft = '100px';
+            e.style.opacity = '.1';
+            setTimeout(f=> {
+                // tar
+            e.style.display = 'none';
+            }, 1000);
+    })
 })*/
+
+
+    // Edit Task
+    /*let txt=document.querySelectorAll('.val');
+    txt.forEach(e=>{
+        e.onclick=()=>{
+            
+            e.classList.toggle("input")
+            e.classList.toggle("is-small")
+
+        }
+    })*/
 
 
 }
